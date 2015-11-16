be5c999e9e81bc493835bef9ac7405afeb49e210
RaRuP777 <pedro.jonathan.ramos@gmail.com>
2015-11-16 22:07:34 +0100

    16/11/15

--- Tema4/js/js.js
+++ Tema4/js/js.js
@@ -3,10 +3,14 @@ var texto  = document.getElementById("texto");
 var resultado ="";
 }
 
-function t1(){
+function t11(){
 	var styles = ["Jazz", "Blues"];
-	styles.push("Rock'n'Roll");
-	styles[styles.length-2] = "Classic";
+	resultado = styles.push("Rock'n'Roll");
+	texto.innerHTML = resultado;
+}
+
+function t12(){
+	var styles = ["Jazz", "Blues"];
 	resultado = styles.pop();
 	texto.innerHTML = resultado;
 }
@@ -28,17 +32,129 @@ function find(array, value) {
 }
 
 function t32(){
-	var arr = ["a", 1, "b", 2];
+	var array = ["a", 1, "b", 2];
 	arr = filterNumeric(arr);
-	resultado = arr;
+	resultado = array;
 	texto.innerHTML = resultado;
 }
 
 function filterNumeric(array){
-	var array2 = new Array;
-	for (var i=0; i<array.length; i++){
-		if array[i].filter
-			array2.push(array[i];
+	var array2 = array.filter(Number);
+	return array2;
+}
+
+function t4(){
+	var fruits = "Apple,Orange,Peach";
+	var array = fruits.split(',');
+	resultado = array;
+	texto.innerHTML = resultado;
+}
+
+function t5(){
+	var array = ["a", "b"];
+	array.push( function() { alert(this) } );
+	array[array.length-1]();
+}
+
+function t61(){
+	var fruits = [];
+	fruits[1] = 'Peach';
+	fruits[99] = 'Apple';
+	resultado = fruits.length;
+	texto.innerHTML = resultado;
+}
+
+function t62(){
+	var fruits = [];
+	fruits[2] = 'Peach';
+	fruits[5] = 'Apple';
+	resultado = fruits;
+	texto.innerHTML = resultado;
+}
+
+function t63(){
+	var fruits = [];
+	fruits[1] = 'Peach';
+	fruits[9] = 'Apple';
+	fruits.pop();
+	fruits.pop();
+	resultado = fruits;
+	texto.innerHTML = resultado;
+}
+
+function t71(){
+	var array = ["Go", "to", "home"];
+	delete array[1];
+	resultado = array[1];
+	texto.innerHTML = resultado;
+}
+
+function t72(){
+	var array = ["Go", "to", "home"];
+	array.splice(1, 1);
+	resultado = array;
+	texto.innerHTML = resultado;
+}
+
+function t73(){
+	var array = ["Go", "to", "home", "now"];
+	array.splice(0, 3, "Come", "here");
+	resultado = array;
+	texto.innerHTML = resultado;
+}
+
+function t74(){
+	var array = ["Why", "learn", "JavaScript"];
+	var array2 = array.slice(0,2);
+	resultado = array2;
+	texto.innerHTML = resultado;
+}
+
+function t8(){
+	var array = ["my","site","com"];
+	resultado = array.reverse();
+	texto.innerHTML = resultado;
+}
+
+function t9(){
+	var array = [ 1, 2, 15 ];
+	resultado = array.sort();
+	texto.innerHTML = resultado;
+}
+
+function t101(){
+	var array = new Array(2,3);
+	resultado = array;
+	texto.innerHTML = resultado;
+}
+
+function t102(){
+	var array = [
+		[1,2,3],
+		[4,5,6],
+		[7,8,9]
+	];
+	resultado = array[1][1];
+	texto.innerHTML = resultado;
+}
+
+function suma100Primos(){
+	var total=0;
+	for (var i = 1; i<101; i++){
+		if(esPrimo(i))
+			total+=i;
 	}
-	return
+	texto.innerHTML = total;
+}
+
+function esPrimo(numero){
+	var a=0;
+    for (var i = 1; i < numero; i++) {
+        if (numero%i==0)
+        	a++;
+    }
+    if (a==2)
+    	return true;
+    else
+    	return false;
 }

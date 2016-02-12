73033dc83f01955a020e91b6f6391e33bc7de719
RaRuP777 <pedro.jonathan.ramos@gmail.com>
2016-02-12 21:51:31 +0100

    12/02/2016

---
+++ DWECL/Tema7-3/js/32.js
@@ -0,0 +1,23 @@
+function muestraRestantesCola(){
+	var numFuncionesEnCola = $("#micapa").queue().length;
+	$("#mensaje").text("En el momento de hacer el último clic en los botones hay " +
+	numFuncionesEnCola + " funciones de efectos en cola");
+}
+
+$(document).ready(function(){
+	$("#botonfade").click(function(){
+		capa = $("#micapa");
+		capa.fadeOut(500);
+		capa.fadeIn(500);
+		muestraRestantesCola();
+	});
+	$("#botonslide").click(function(){
+		capa = $("#micapa");
+		capa.slideUp(500);
+		capa.slideDown(500);
+		muestraRestantesCola();
+	});
+	$("#botontamanocola").click(function(){
+		muestraRestantesCola();
+	});
+});

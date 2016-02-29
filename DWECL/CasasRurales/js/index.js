de566b765a5e69a776242bb0fba77c059816db38
RaRuP777 <pedro.jonathan.ramos@gmail.com>
2016-02-29 23:24:39 +0100

    29/02/2016

---
+++ DWECL/CasasRurales/js/index.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function(){
+
+	comprobarCookies();
+	
+	function comprobarCookies(){
+		if(!navigator.cookieEnabled)
+			alert("Debe tener las cookies activadas para utilizar correctamente esta página");
+	};
+
+    $("#titulo1").click(function(){
+        $("#descripcion1").slideDown("slow");
+        $("#casa1").fadeTo("slow", 0.5);
+    });
+
+    $("#titulo2").click(function(){
+        $("#descripcion2").slideDown("slow");
+        $("#casa2").fadeTo("slow", 0.5);
+    });
+
+    $("#titulo3").click(function(){
+        $("#descripcion3").slideDown("slow");
+        $("#casa3").fadeTo("slow", 0.5);
+    });
+
+    $("#titulo4").click(function(){
+        $("#descripcion4").slideDown("slow");
+        $("#casa4").fadeTo("slow", 0.5);
+    });
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'showImageNumberLabel': false,
+        'positionFromTop': 120,
+        'disableScrolling': true
+    })
+
+
+});

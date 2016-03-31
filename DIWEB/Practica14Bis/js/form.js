4a5755b7b5a21bc29ccdf10106b8ffcc887073d1
RaRuP777 <pedro.jonathan.ramos@gmail.com>
2016-03-31 18:31:07 +0200

    31/03/2016

---
+++ DIWEB/Practica14Bis/js/form.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function(){
+
+    //datos del formulario
+    var nombre = $("#nombre");
+    var email = $("#email");
+    var mensaje = $("#mensaje");
+
+    //mensajes de error
+    var errorNombre = $("#errorNombre");
+    var errorEmail = $("#errorEmail");
+
+    //evento presionar botón crear
+    $("#enviar").click(function(){
+        if(validarFormulario()){
+            crearCookie();
+            alert("Mensaje enviado");
+        }
+    });
+
+    //evento presionar botón limpiar
+    $("#limpiar").click(function (){
+        nombre.html("");
+        email.html("");
+        mensaje.html("");
+
+        errorNombre.html("");
+        errorEmail.html("");
+    });
+
+    //cookies
+    function crearCookie(){
+        $.cookie("nombre",$('#nombre').val(),{expires: 365});
+        $.cookie("email",$('#email').val(),{expires: 365});
+        $.cookie("mensaje",$('#mensaje').val(),{expires: 365});
+    }
+
+});
+
